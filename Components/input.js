178d13f07fe5399db1dac8393e28a43e138ac7c7
jinzhang01 <144280070+jinzhang01@users.noreply.github.com>
2024-07-04 15:08:14 -0700

    refactor the code to have input component

--- Components/input.js
+++ Components/input.js
@@ -2,11 +2,12 @@ import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 import { useState } from 'react'
 
-const[text, setText] = useState("");
+
 const Input = () => {
+    const[text, setText] = useState("");
   return (
     <View>
-    <TextInput>  
+    <TextInput  
     value={text}
     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
     placeholder="Type here to start!"
@@ -16,7 +17,7 @@ const Input = () => {
       // console.log(text)
       // console.log(setText)
     }}
-    </TextInput>
+    />
         <Text>you typed: {text}</Text>
     </View>
   )
