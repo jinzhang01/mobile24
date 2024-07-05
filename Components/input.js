d51d6dcc4cdccd0805a85935183ad0b3aac57192
jinzhang01 <144280070+jinzhang01@users.noreply.github.com>
2024-07-04 17:28:01 -0700

    implement focus

--- Components/input.js
+++ Components/input.js
@@ -2,23 +2,32 @@ import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 import { useState } from 'react'
 
-
 const Input = () => {
-    const[text, setText] = useState("");
+const[text, setText] = useState("");
+const[isEdited, setEdited] = useState(false);
+
   return (
     <View>
-    <TextInput  
+    <TextInput 
+    autoFocus={true} 
+ 
     value={text}
     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
     placeholder="Type here to start!"
     // onChangeText={text => setText(text)}
+    
     onChangeText ={function(changedText){
       setText(changedText);
+      setEdited(!isEdited);
       // console.log(text)
       // console.log(setText)
+    
     }}
+    
     />
-        <Text>you typed: {text}</Text>
+
+    <Text>you typed: {text}</Text>
+    {isEdited ? <Text>Thank You</Text> : null}
     </View>
   )
 }
