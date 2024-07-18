24bf2e8398eb59f2e48a8175320d3017299e36f5
jinzhang01 <144280070+jinzhang01@users.noreply.github.com>
2024-07-18 13:36:58 -0700

    add pressable

--- Components/Goalitem.js
+++ Components/Goalitem.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button} from 'react-native'
+import { View, Text, StyleSheet, Button, Pressable} from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 
@@ -8,12 +8,20 @@ const Goalitem = ({passItem, deleteHandler, pressHandler}) => {
 
   return (
     <View style={styles.textContainer}>
+      <Pressable
+        style={styles.pressable}
+        onPress={()=>navigation.navigate('Details', {passItem})}
+      >
         <Text style={styles.textStyle}>{text}</Text>
         
         <Button color="red" title="x" onPress={()=>deleteHandler(id)}/> 
-        <Button color="black" title="i" onPress={()=>navigation.navigate('Details', {passItem})
+
+        {/* <Button color="black" title="i" onPress={()=>navigation.navigate('Details', {passItem})
           //pass the goal object. 
-        }/>
+        }/> */}
+
+
+        </Pressable>
     </View>
   )
 }
@@ -24,7 +32,7 @@ textContainer: {
     backgroundColor: 'lightblue',
     marginVertical: 10,
     borderRadius: 10,
-    padding: 10, 
+    
     flexDirection: "row",
     justifyContent: "space-between",
   },
@@ -32,7 +40,14 @@ textContainer: {
     fontSize: 24, 
     marginVertical: 5,
     margin: 10
-  }
+  },
+  pressable: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: 'lightblue',
+    padding: 10, 
+  },
 })
 
 export default Goalitem
