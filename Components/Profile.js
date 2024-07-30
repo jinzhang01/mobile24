2d9b71302847e94fc3603fb11f415df6e6810655
jinzhang01 <144280070+jinzhang01@users.noreply.github.com>
2024-07-30 14:55:57 -0700

    logout works

--- Components/Profile.js
+++ Components/Profile.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { auth } from '../firebase/firebaseSetup';
+import { Button } from 'react-native';
 
 const Profile = () => {
   const userId = auth.currentUser.uid;
+  
+  function signOut (navigation) {
+    auth.signOut().then(() => {
+      console.log("User signed out");
+    //   navigation.navigation("Login");
+    }).catch((error) => {
+      console.error("Error signing out:", error);
+    });}
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Profile</Text>
       <Text>ID: {userId}</Text>
+
+      <Button title="Sign out" onPress={() => signOut()} />
     </View>
+
   );
 }
 
