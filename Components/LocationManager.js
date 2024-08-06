63d24ea3ea16799c50fe385fd3277de61f72ef8a
jinzhang01 <144280070+jinzhang01@users.noreply.github.com>
2024-08-06 14:59:25 -0700

    embed the map and location on home page

--- Components/LocationManager.js
+++ Components/LocationManager.js
@@ -1,6 +1,10 @@
-import { View, Button, Text } from 'react-native'; // Import the Text component
+import { View, Button, Text, Image } from 'react-native'; // Import the Image component
 import React, { useState, useEffect } from 'react';
 import * as Location from 'expo-location'; // Import the Location module from expo-location
+import { mapsApiKey } from '@env';
+import { Dimensions } from 'react-native';
+
+
 
 const LocationManager = () => {
   const [status, requestPermission] = Location.useForegroundPermissions();
@@ -39,6 +43,12 @@ const LocationManager = () => {
         <View>
           <Text>Latitude: {location.latitude}</Text>
           <Text>Longitude: {location.longitude}</Text>
+          <Image
+            style={{ width: 400, height: 200 }}
+            source={{
+              uri: `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${location.latitude},${location.longitude}&key=${mapsApiKey}`
+            }}
+          />
         </View>
       )}
     </View>
